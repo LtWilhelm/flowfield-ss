2fb9d85e981492a1a11e9898614dcda2d0b617f9
Mason Short <ironicallyshort737@hotmail.com>
2020-04-28 15:42:23 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+let inc = 0.1
+let cols, rows;
+let scl = 20;
+
+let zoff = 0
+
+let fr
+
+let particles = [];
+let flowfield = [];
+
+function setup() {
+  createCanvas(innerWidth, innerHeight - 10)
+  cols = floor(width / scl)
+  rows = floor(height / scl)
+  // fr = createP()
+
+  for (let i = 0; i < 1000; i++) {
+    particles[i] = new Particle();
+  }
+}
+
+function draw() {
+  colorMode('rgb')
+  background(0, 10);
+  // colorMode('hsb')
+  let yoff = 0;
+  for (let y = 0; y < rows; y++) {
+    let xoff = 0;
+    for (let x = 0; x < cols; x++) {
+      let index = (x + y * cols);
+      let angle = noise(xoff, yoff, zoff) * TWO_PI *4
+      let v = p5.Vector.fromAngle(angle);
+      v.setMag(.5)
+      flowfield[index] = v;
+      stroke(0, 50)
+      push();
+      translate(x*scl, y*scl);
+      // rotate(v.heading())
+      // strokeWeight(1)
+      // line(0, 0, scl, 0)
+      pop();
+      xoff += inc
+    }
+    yoff += inc
+  }
+  // noLoop();
+  zoff += 0.001
+  particles.forEach(e => e.follow(flowfield))
+  particles.forEach(e => e.update())
+  particles.forEach(e => e.show())
+  particles.forEach(e => e.edges())
+  // fr.html(floor(frameRate()))
+  if (zoff >= .11) noLoop()
+}
---
+++ particle.js
@@ -0,0 +1,43 @@
+function Particle() {
+  this.pos = createVector(random(width), random(height))
+  this.vel = createVector(0, 0)
+  this.acc = createVector(0, 0)
+  this.maxSpeed = 10;
+  this.color = 0
+
+  this.update = function () {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed)
+    this.pos.add(this.vel)
+    this.acc.mult(0)
+    this.color += 1
+    if (this.color > 360) this.color = 0
+  }
+
+  this.applyForce = function (force) {
+    this.acc.add(force)
+  }
+
+  this.show = function () {
+    colorMode("hsb")
+    stroke(this.color, 100, 75, 75);
+    strokeWeight(1)
+    point(this.pos.x, this.pos.y);
+  }
+
+  this.edges = function () {
+    if (this.pos.x > width) this.pos.x = 0
+    if (this.pos.x < 0) this.pos.x = width
+    if (this.pos.y > height) this.pos.y = 0
+    if (this.pos.y < 0) this.pos.y = height
+  }
+
+  this.follow = function(vectors) {
+    let x = floor(this.pos.x/scl)
+    let y = floor(this.pos.y/scl)
+    let i = x + y * cols;
+    let force = vectors[i]
+
+    this.applyForce(force);
+  }
+}
